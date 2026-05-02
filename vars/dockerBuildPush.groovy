88ee3b659974874a28b617e751f758fa160bf509
yakubpasha-mohd <43091747+yakubpasha-mohd@users.noreply.github.com>
2026-05-03 01:43:18 +0530

    Update dockerBuildPush.groovy
    Update dockerBuildPush.groovy

--- vars/dockerBuildPush.groovy
+++ vars/dockerBuildPush.groovy
@@ -1,10 +1,18 @@
-def call(Map config) {
+def call(Map config = [:]) {
+
+    if (!config.service || !config.registry || !config.tag) {
+        error "Missing required config: service/registry/tag"
+    }
 
     def image = "${config.registry}/${config.service}:${config.tag}"
 
-    echo "Building image: ${image}"
+    echo "🐳 Building image: ${image}"
 
     dir("services/${config.service}") {
+
+        if (!fileExists("Dockerfile")) {
+            error "Dockerfile not found for ${config.service}"
+        }
 
         sh """
             docker build \
@@ -14,7 +22,6 @@ def call(Map config) {
         """
     }
 
-    echo "Pushing image: ${image}"
-
+    echo "🚀 Pushing image: ${image}"
     sh "docker push ${image}"
 }
