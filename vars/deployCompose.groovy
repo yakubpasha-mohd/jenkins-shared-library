5c30e921428a69c160eedbd81a5c54ec75b5d49d
yakubpasha-mohd <43091747+yakubpasha-mohd@users.noreply.github.com>
2026-05-03 00:18:50 +0530

    Update deployCompose.groovy
    Update deployCompose.groovy

--- vars/deployCompose.groovy
+++ vars/deployCompose.groovy
@@ -2,15 +2,18 @@ def call(Map config) {
 
     stage("Deploy ${config.environment}") {
 
-        for (svc in config.services) {
+        dir("${env.WORKSPACE}") {   // ✅ ensure root directory
+
+            for (svc in config.services) {
+                sh """
+                sed -i 's|${config.registry}/${svc}:latest|${config.registry}/${svc}:${config.tag}|g' docker-compose.${config.environment}.yml
+                """
+            }
+
             sh """
-            sed -i 's|${config.registry}/${svc}:latest|${config.registry}/${svc}:${config.tag}|g' docker-compose.${config.environment}.yml
+                docker-compose -f docker-compose.${config.environment}.yml down || true
+                docker-compose -f docker-compose.${config.environment}.yml up -d
             """
         }
-
-        sh """
-            docker-compose -f docker-compose.${config.environment}.yml down || true
-            docker-compose -f docker-compose.${config.environment}.yml up -d
-        """
     }
 }
