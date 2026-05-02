856dab9c18da5431802b8f05d0c94327e4dfd874
yakubpasha-mohd <43091747+yakubpasha-mohd@users.noreply.github.com>
2026-05-03 00:13:30 +0530

    Update buildService.groovy
    Update buildService.groovy

--- vars/buildService.groovy
+++ vars/buildService.groovy
@@ -1,30 +1,27 @@
 def call(String svc) {
 
-    stage("Build ${svc}") {
+    if (svc == 'all') {
+        error "Invalid service name 'all'"
+    }
 
-        if (svc == 'all') {
-            error "Invalid service name 'all'"
-        }
+    dir("services/${svc}") {
 
-        dir("services/${svc}") {
+        sh '''
+            echo "Building service: $(pwd)"
 
-            sh '''
-                echo "Building service: $(pwd)"
+            if [ -f pom.xml ]; then
+                echo "Detected Maven project"
+                mvn clean package -DskipTests=false -U
 
-                if [ -f pom.xml ]; then
-                    echo "Detected Maven project"
-                    mvn clean package -DskipTests=false -U
+            elif [ -f package.json ]; then
+                echo "Detected Node project"
+                npm ci
+                npm run build || echo "No build script"
 
-                elif [ -f package.json ]; then
-                    echo "Detected Node project"
-                    npm ci
-                    npm run build || echo "No build script"
-
-                else
-                    echo "Unknown project type"
-                    exit 1
-                fi
-            '''
-        }
+            else
+                echo "Unknown project type"
+                exit 1
+            fi
+        '''
     }
 }
