f753648a5068e30b1073e4fa0a47f6908abe469e
yakubpasha-mohd <43091747+yakubpasha-mohd@users.noreply.github.com>
2026-05-03 00:14:13 +0530

    Update dockerBuildPush.groovy
    Update dockerBuildPush.groovy

--- vars/dockerBuildPush.groovy
+++ vars/dockerBuildPush.groovy
@@ -1,21 +1,18 @@
 def call(Map config) {
 
-    stage("Docker Build ${config.service}") {
-        dir("services/${config.service}") {
-            sh "docker build -t ${config.registry}/${config.service}:${config.tag} ."
-        }
+    dir("services/${config.service}") {
+        sh "docker build -t ${config.registry}/${config.service}:${config.tag} ."
     }
 
-    stage("Docker Push ${config.service}") {
-        withCredentials([usernamePassword(
-            credentialsId: 'docker-cred',
-            usernameVariable: 'DOCKER_USER',
-            passwordVariable: 'DOCKER_PASS'
-        )]) {
-            sh """
-                echo \$DOCKER_PASS | docker login -u \$DOCKER_USER --password-stdin
-                docker push ${config.registry}/${config.service}:${config.tag}
-            """
-        }
+    withCredentials([usernamePassword(
+        credentialsId: 'docker-cred',
+        usernameVariable: 'DOCKER_USER',
+        passwordVariable: 'DOCKER_PASS'
+    )]) {
+
+        sh """
+            echo \$DOCKER_PASS | docker login -u \$DOCKER_USER --password-stdin
+            docker push ${config.registry}/${config.service}:${config.tag}
+        """
     }
 }
