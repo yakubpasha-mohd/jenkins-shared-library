821b3f0e0dd3ed8e3e420acacf51efb8bb8dfcd1
yakubpasha-mohd <43091747+yakubpasha-mohd@users.noreply.github.com>
2026-05-03 16:17:00 +0530

    Update sonarScan.groovy
    Update sonarScan.groovy

--- vars/sonarScan.groovy
+++ vars/sonarScan.groovy
@@ -1,13 +1,16 @@
 def call(String svc) {
-    stage("Sonar Scan ${svc}") {
-        dir("services/${svc}") {
-            withSonarQubeEnv('SonarQube') {
-                sh """
-                    mvn sonar:sonar \
-                    -Dsonar.projectKey=${svc} \
-                    -Dsonar.projectName=${svc}
-                """
-            }
+
+    echo "🔍 Running SonarQube scan for ${svc}"
+
+    dir("services/${svc}") {
+
+        withSonarQubeEnv('SonarQube') {
+
+            sh """
+                mvn sonar:sonar \
+                -Dsonar.projectKey=${svc} \
+                -Dsonar.projectName=${svc}
+            """
         }
     }
 }
